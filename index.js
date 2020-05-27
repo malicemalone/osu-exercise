b304d746d219a50c985d7756536cfe7c5f350b0a
David Jansen <jansedav@oregonstate.edu>
2020-05-27 11:45:37 -0700

    Adding initial project

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => console.log(`Server listening at port:${port}`))
